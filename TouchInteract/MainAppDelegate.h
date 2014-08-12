ffd2b0322243f9c40936c01b8d957a72007a6c82
Rahul Morade <rahul@maxdigi.com>
2014-08-12 19:04:14 +0530

    Initial Commit

---
+++ TouchInteract/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  TouchInteract
+//
+//  Created by Rahul on 12/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchInteractTests/TouchInteractTests.m
@@ -0,0 +1,34 @@
+//
+//  TouchInteractTests.m
+//  TouchInteractTests
+//
+//  Created by Rahul on 12/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TouchInteractTests : XCTestCase
+
+@end
+
+@implementation TouchInteractTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
